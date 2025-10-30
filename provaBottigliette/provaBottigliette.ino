b34b586fc7ca3aad74144d41ff50479841d38b7f
ZioIko <federicorossano1996@gmail.com>
2025-10-30 17:48:43 +0100

    changes

--- provaBottigliette/provaBottigliette.ino
+++ provaBottigliette/provaBottigliette.ino
@@ -25,7 +25,7 @@ unsigned long totalTime2;
 // Other variables initialization
 unsigned long timeDifference;
 unsigned long timeWin = 0.25; //initialize time window: 250 ms
-int n_otuside = 0; // Counter for number of consecutive trials where timeDifference is outside time window
+int n_outside = 0; // Counter for number of consecutive trials where timeDifference is outside time window
 int n_within = 0; // Counter for number of consecutive trials where timeDifference is within time window
 // Utily functions
 
@@ -49,8 +49,24 @@ int n_within = 0; // Counter for number of consecutive trials where timeDifferen
   return timeWin;
  }
 
- int isConsecutive(unsigned long timeDifference, int n_outside, int n_within, unsigned long timeWin)
+  int isConsecutive(unsigned long timeDifference, int n_outside,int n_within, unsigned long timeWin)
+  {
+    if (timeDifference > timeWin && n_within == 1)
+    {
+      n_outside = 0;
+      return n_outside;
+    }
+    if (timeDifference <= timeWin && n_outside ==1)
+    {
+      n_within = 0;
+      return n_within;
+    }
+
+  }
+ 
+ int changeTimeWin(unsigned long timeDifference, int n_outside, int n_within, unsigned long timeWin)
  {
+
   if (timeDifference <= timeWin) 
   {
     n_within = n_within + 1;
@@ -69,6 +85,10 @@ int n_within = 0; // Counter for number of consecutive trials where timeDifferen
   }
   else 
   {
+    digitalWrite(11,"HIGH");
+    delay(500);
+    digitalWrite(11,LOW);
+
      n_outside = n_outside + 1;
      if (n_outside == 2)
      {
@@ -94,6 +114,8 @@ void setup() {
  pinMode(buttonPin2,INPUT);
  pinMode(touchPinUP2,INPUT);
  pinMode(touchPinDOWN2,INPUT);
+ pinMode(11,OUTPUT); // pin attached to LEDs for feedback on grasping synchrony
+ 
 
 }
 
@@ -101,31 +123,30 @@ void loop() {
   int buttonState1 = digitalRead(buttonPin1);
   int touchStateUP1 = digitalRead(touchPinUP1);
   int touchStateDOWN1 = digitalRead(touchPinDOWN1);
-  int buttonState2 = digitalRead(buttonPin1);
-  int touchStateUP2 = digitalRead(touchPinUP1);
-  int touchStateDOWN2 = digitalRead(touchPinDOWN1);
+  int buttonState2 = digitalRead(buttonPin2);
+  int touchStateUP2 = digitalRead(touchPinUP2);
+  int touchStateDOWN2 = digitalRead(touchPinDOWN2);
   // ____________________________________________
   // ----------------FIRST BOTTLE----------------
   // ____________________________________________
   // Start record time when button is not pressed
   if (buttonState1 == HIGH && (!touchUP1 && !touchDOWN1)) {
   startTime1 = millis();
-  Serial.print("Start time: ");
-  Serial.println(startTime1);
+  
   buttonReleased1 = true;
   }
 
   // First bottle, up part
   if (touchStateUP1 == HIGH && buttonReleased1 == true && touchUP1 == false) {
     graspingTime1 = GetTime1(startTime1);
-    Serial.print("Grasping timeUP: ");
+    Serial.print("Grasping time UP1:");
     Serial.println(graspingTime1);
     touchUP1 = true;
   }
 
   if (buttonState1 == HIGH && touchUP1 == true && buttonReleased1 == true) {
     totalTime1 = GetTime1(startTime1);
-    Serial.print("Total time UP: ");
+    Serial.print("Total time UP1:");
     Serial.println(totalTime1);
     touchUP1 = false;
     buttonReleased1 = false;
@@ -135,14 +156,14 @@ void loop() {
 
   if (touchStateDOWN1 == HIGH && buttonReleased1 == true && touchDOWN1 == false) {
     graspingTime1 = GetTime1(startTime1);
-    Serial.print("Grasping time DOWN: ");
+    Serial.print("Grasping time DOWN1:");
     Serial.println(graspingTime1);
     touchDOWN1 = true;
   }
 
   if (buttonState1 == HIGH && touchDOWN1 == true && buttonReleased1 == true) {
     totalTime1 = GetTime1(startTime1);
-    Serial.print("Total time DOWN: ");
+    Serial.print("Total time DOWN1:");
     Serial.println(totalTime1);
     touchDOWN1 = false;
     buttonReleased1 = false;  
@@ -155,22 +176,20 @@ void loop() {
   // Start record time when button is not pressed
   if (buttonState2 == HIGH && (!touchUP2 && !touchDOWN2)) {
   startTime2 = millis();
-  Serial.print("Start time: ");
-  Serial.println(startTime2);
   buttonReleased2 = true;
   }
 
   // Second bottle, up part
   if (touchStateUP2 == HIGH && buttonReleased2 == true && touchUP2 == false) {
     graspingTime2 = GetTime2(startTime2);
-    Serial.print("Grasping timeUP: ");
+    Serial.print("Grasping time UP2:");
     Serial.println(graspingTime2);
     touchUP2 = true;
   }
 
   if (buttonState2 == HIGH && touchUP2 == true && buttonReleased2 == true) {
     totalTime2 = GetTime2(startTime2);
-    Serial.print("Total time UP: ");
+    Serial.print("Total time UP2:");
     Serial.println(totalTime2);
     touchUP2 = false;
     buttonReleased2 = false;
@@ -180,14 +199,14 @@ void loop() {
 
   if (touchStateDOWN2 == HIGH && buttonReleased2 == true && touchDOWN2 == false) {
     graspingTime2 = GetTime2(startTime2);
-    Serial.print("Grasping time DOWN: ");
+    Serial.print("Grasping time DOWN2:");
     Serial.println(graspingTime2);
     touchDOWN2 = true;
   }
 
   if (buttonState2 == HIGH && touchDOWN2 == true && buttonReleased2 == true) {
     totalTime2 = GetTime2(startTime2);
-    Serial.print("Total time DOWN: ");
+    Serial.print("Total time DOWN2:");
     Serial.println(totalTime2);
     touchDOWN2 = false;
     buttonReleased2 = false;  
@@ -197,9 +216,10 @@ void loop() {
   if (graspingTime1 > 0 && graspingTime2 > 0)
   {
     timeDifference = abs(graspingTime1 - graspingTime2);
-    Serial.print("time difference between grasps :");
+    Serial.print("time difference between grasps:");
     Serial.println(timeDifference);
     isConsecutive(timeDifference,n_outside,n_within,timeWin);
+    changeTimeWin(timeDifference,n_outside,n_within,timeWin);
     graspingTime1 = 0;
     graspingTime2 = 0;
   }
